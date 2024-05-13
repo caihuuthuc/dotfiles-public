eb25353c7da8b8ca5bafc9350f5585e598f71d7a
Thuc Cai Huu <huuthuc997@gmail.com>
2024-05-13 10:40:47 +0700

    Add files via upload

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,20 @@
+return {
+  { "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  { "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        --ensure_installed = {"clangd"}
+      })
+    end
+  },
+  { "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.clangd.setup({cmd = {"/usr/bin/clangd"}})
+    end 
+  }
+}
